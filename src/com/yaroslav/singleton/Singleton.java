a56b8eddda1547c2734b0a5e48a41538b5dd6b0e
StarovoytYaroslav <Starovoyt.yaroslav@gmail.com>
2020-01-15 23:57:05 +0200

    finished singleton

--- src/com/yaroslav/singleton/Singleton.java
+++ src/com/yaroslav/singleton/Singleton.java
@@ -2,21 +2,18 @@ package com.yaroslav.singleton;
 
 public class Singleton {
 	public static int counter = 0;
-	private static Singleton instance = null;
+	private static volatile Singleton instance = null;
 	private Singleton() {
 		counter ++;
 	}
 
-	//	public static Singleton getInstance() {
-	//		if (instance == null) {
-	//			instance = new Singleton();
-	//		}
-	//		return instance;
-	//	}
-
-	public static synchronized Singleton getInstance() {
-		if (instance == null)
-			instance = new Singleton();
-			return instance;
+	public static Singleton getInstance() {
+		if (instance == null) {
+			synchronized (Singleton.class) {
+				if (instance == null)
+					instance = new Singleton();
+			}
+		}
+		return instance;
 	}
 }
