830981843995db3d356749c7603116fdb6a8abbc
StarovoytYaroslav <Starovoyt.yaroslav@gmail.com>
2020-01-15 17:12:26 +0200

    First way of fixing problem with multithreading in singleton

--- src/com/yaroslav/singleton/Singleton.java
+++ src/com/yaroslav/singleton/Singleton.java
@@ -2,15 +2,15 @@ package com.yaroslav.singleton;
 
 public class Singleton {
 	public static int counter = 0;
-	private static Singleton instance;
+	private static Singleton instance = new Singleton();
 	private Singleton() {
 		counter ++;
 	}
 	
 	public static Singleton getInstance() {
-		if (instance == null) {
-			instance = new Singleton();
-		}
+//		if (instance == null) {
+//			instance = new Singleton();
+//		}
 		return instance;
 	}
 	
