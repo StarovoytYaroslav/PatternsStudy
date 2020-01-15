0ec7efd451c0eb3319a0fbd3fd7cd92ca97abb2a
StarovoytYaroslav <Starovoyt.yaroslav@gmail.com>
2020-01-15 23:43:01 +0200

    Fixed with synchronized

--- src/com/yaroslav/singleton/Singleton.java
+++ src/com/yaroslav/singleton/Singleton.java
@@ -2,16 +2,21 @@ package com.yaroslav.singleton;
 
 public class Singleton {
 	public static int counter = 0;
-	private static Singleton instance = new Singleton();
+	private static Singleton instance = null;
 	private Singleton() {
 		counter ++;
 	}
-	
-	public static Singleton getInstance() {
-//		if (instance == null) {
-//			instance = new Singleton();
-//		}
-		return instance;
+
+	//	public static Singleton getInstance() {
+	//		if (instance == null) {
+	//			instance = new Singleton();
+	//		}
+	//		return instance;
+	//	}
+
+	public static synchronized Singleton getInstance() {
+		if (instance == null)
+			instance = new Singleton();
+			return instance;
 	}
-	
 }
